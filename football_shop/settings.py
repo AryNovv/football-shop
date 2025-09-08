ff42ec07d59115f2a26b8606b7c0d51478c359d5
AryNov <arya.nov.pratama@gmail.com>
2025-09-08 23:02:01 +0700

    landasan part 2

--- football_shop/settings.py
+++ football_shop/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = 'django-insecure-8qosae8es%qkg&e37um$--48huemsn%4*bi#!l!)3!%sfl-=co
 PRODUCTION = os.getenv('PRODUCTION', 'False').lower() == 'true'
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1","arya-novalino-footballshop.pbp.cs.ui.ac.id"]
 
 
 # Application definition
