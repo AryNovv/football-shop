9f10d7770ec9b9dad2eb30b66429341837473a04
AryNov <arya.nov.pratama@gmail.com>
2025-09-22 21:49:01 +0700

    tugas 4 ver 1

--- main/models.py
+++ main/models.py
@@ -1,7 +1,11 @@
 import uuid
 from django.db import models
+from django.contrib.auth.models import User
+
 
 class Produk(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+    
     CATEGORY_CHOICES = [
         ('boots', 'Boots'),
         ('knee-guards', 'Knee-guards'),
