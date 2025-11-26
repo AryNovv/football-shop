e0b6743ad50b61d640e1e7121e221befdf7dc156
AryNov <arya.nov.pratama@gmail.com>
2025-11-26 08:19:54 +0700

    Add create-flutter endpoint and fix JSON response

--- main/urls.py
+++ main/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from main.views import show_main, create_listing, show_catalog,show_xml,show_json,show_xml_by_id, show_json_by_id,register,login_user,logout_user,edit_listing,delete_Listing,add_listing_entry_ajax,sort_listing_with_ajax
+from main.views import show_main, create_listing, show_catalog,show_xml,show_json,show_xml_by_id, show_json_by_id,register,login_user,logout_user,edit_listing,delete_Listing,add_listing_entry_ajax,sort_listing_with_ajax,create_product_flutter, proxy_image
 
 app_name = 'main'
 
@@ -18,5 +18,7 @@ urlpatterns = [
     path('news/<uuid:id>/delete', delete_Listing, name='delete_Listing'),
     path('create-listing-ajax', add_listing_entry_ajax, name='add_listing_entry_ajax'),
     path('sort-listing-ajax', sort_listing_with_ajax, name='sort_listing_with_ajax'),
+    path('create-flutter/', create_product_flutter, name='create_product_flutter'),
+    path('proxy-image/', proxy_image, name='proxy_image'),
 
 ]
